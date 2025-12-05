28fd353c46132e0658873578b30680769831a3f3
goesting <75796795+goesting@users.noreply.github.com>
2025-12-05 17:06:22 +0100

    Update Day05.rb
    cleaning

--- Day05.rb
+++ Day05.rb
@@ -6,15 +6,8 @@ ingredients = ingredients.split.map &:to_i
 puts ingredients.count{|ing| ranges.any?{|r| r.cover?(ing)}}
 
 #part2
-lvl = 0
-result = 0
+lvl = result = 0
 range_start = range_end = -1
-#LVL: Keep track of the amount of ranges that are currently covering the same piece
-#     -1 for each extra layer of overlap, +1 whenever one closes
-#      done this way to make sorting easier
-#range_start: beginning of this overlap range
-#range_end: gets set when all overlaps stop (lvl resets to 0)
-#result: when all overlaps are done(lvl is back to 0), add the total size of overlapping ranges
 
 ranges.map{|rng| [[rng.min,-1],[rng.max,1]]}.flatten(1).sort.each{|val,status| 
   
